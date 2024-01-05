b81b39280c16a26794cf9801f6cf17a8688b587d
Andreasaja <under.block22@gmail.com>
2024-01-05 10:55:07 +0700

    registerpage

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,10 +17,14 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+
+    protected $primaryKey = 'id';
+    protected $table = 'users';
     protected $fillable = [
         'name',
         'email',
         'password',
+        'role'
     ];
 
     /**
